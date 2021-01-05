2069dc36e1fb2b2da7fc22eb20a1599c90b13949
robingta <robin@primtechs.com>
2021-01-05 21:50:37 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import random
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hey There"
+
+
+@app.route('/submit/temperature/<temperature>')
+def submit_temperature(temperature):
+    fahrenheit = (int(temperature) * 1.8) + 32
+    link = 'http://developer.muthofun.com/sms.php?username=hasanmahmud&password=hAsAn.420&mobiles=01625376336&sms=Critical Temperature&uniccode=1'
+    if fahrenheit >= 100:
+        requests.get(url=link)
+        print("SMS SEND")
+
+    if len(firebase_admin._apps) == 0:
+        cred = credentials.Certificate('./service.json')
+        firebase_admin.initialize_app(cred, {'databaseURL': 'https://healthmonitor-56a4b.firebaseio.com/',
+                                                 'databaseAuthVariableOverride': None})
+    db.reference("temperature").push().set({'value': int(temperature)})
+    return "done"
+
+
+@app.route('/submit/heartbeat/<heartbeat>')
+def submit_heartbeat(heartbeat):
+    heartbeat = random.randrange(81, 97)
+    if len(firebase_admin._apps) == 0:
+        cred = credentials.Certificate('./service.json')
+        firebase_admin.initialize_app(cred, {'databaseURL': 'https://healthmonitor-56a4b.firebaseio.com/',
+                                             'databaseAuthVariableOverride': None, })
+    db.reference("heartbeat").push().set({'value': int(heartbeat)})
+    return "done"
+
+
+if __name__ == "__main__":
+app.run(debug=True)
